bfbabb9cbcd9478b1cda7942ed45389efaf021a6
cyx <1509117274@qq.com>
2026-03-15 15:16:08 +0800

    build(backend): 后端项目初始化

---
+++ backend/Smart/web/web-admin/src/main/java/com/smartcare/AdminWebApplication.java
@@ -0,0 +1,12 @@
+package com.smartcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AdminWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminWebApplication.class, args);
+    }
+}
---
+++ backend/Smart/web/web-admin/src/main/java/com/smartcare/web/admin/mapper/Event.java
@@ -0,0 +1,4 @@
+package com.smartcare.web.admin.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+public interface Event extends BaseMapper<Event> {
+}
