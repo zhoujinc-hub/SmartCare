1cc648bab017fe81b2b40a949eca65fc004de698
cyx <1509117274@qq.com>
2026-04-23 12:59:09 +0800

    build(backend): 后端项目更改

---
+++ backend/src/main/java/com/smartcare/dto/user/RegisterDto.java
@@ -0,0 +1,13 @@
+package com.smartcare.dto.user;
+
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+    private String username;
+    private String phone;
+    // 类型从 Integer 改为 Byte
+    private Byte userType;
+    private String password;
+    private String code;
+}
