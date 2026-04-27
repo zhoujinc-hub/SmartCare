a5ae9156862cb468975946cc175c059c94694d53
cyx <1509117274@qq.com>
2026-04-27 18:00:09 +0800

    build(backend): 后端项目更改

--- backend/src/main/java/com/smartcare/service/FamilyElderService.java
+++ backend/src/main/java/com/smartcare/service/FamilyElderService.java
@@ -14,17 +14,17 @@ import java.util.List;
 public interface FamilyElderService {
 
     /**
-     * 查询当前家属绑定的老人列表
+     * 查询指定家属绑定的老人列表
      */
-    List<ElderVO> listMyElders();
+    List<ElderVO> listMyElders(Long userId);
 
     /**
-     * 添加老人信息，并绑定当前家属
+     * 添加老人信息，并绑定指定家属
      */
-    void addElder(ElderAddDTO dto);
+    void addElder(Long userId, ElderAddDTO dto);
 
     /**
      * 查询某个老人的跌倒事件分页列表
      */
-    PageVo<ElderFallEventVO> listFallEvents(Long elderId, PageQueryDTO pageQuery);
+    PageVo<ElderFallEventVO> listFallEvents(Long userId, Long elderId, PageQueryDTO pageQuery);
 }
